93794a4d3489102523f00ce0ea55450df7847975
yakusugi <yosukeito1987@gmail.com>
2021-07-14 16:10:30 +0900

    Step 6

--- app/src/main/java/com/example/offlinebudgettracker/MainActivity.java
+++ app/src/main/java/com/example/offlinebudgettracker/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.offlinebudgettracker.model.BudgetTrackerDto;
 import com.example.offlinebudgettracker.model.BudgetTrackerViewModel;
@@ -15,18 +16,26 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private BudgetTrackerViewModel budgetTrackerViewModel;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textSample);
 
         budgetTrackerViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this
                 .getApplication())
                 .create(BudgetTrackerViewModel.class);
 
         budgetTrackerViewModel.getAllBudgetTrackingContents().observe(this, budgetTrackerDtos -> {
-            Log.d("TAG", "onCreate: " + budgetTrackerDtos.get(0).getProductName());
+            StringBuilder builder = new StringBuilder();
+            for (BudgetTrackerDto budgetTrackerDto : budgetTrackerDtos) {
+                builder.append(" - ").append(budgetTrackerDto.getDate()).append(" ").append(budgetTrackerDto.getProductName());
+                Log.d("TAG", "onCreate: " + budgetTrackerDto.getProductName());
+            }
+            textView.setText(builder.toString());
+
         });
     }
 }
